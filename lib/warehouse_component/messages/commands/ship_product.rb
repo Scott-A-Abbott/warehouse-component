58f3c2071a280f8bd559af7ce483ae7924180579
Scott Abbott <scott.abbott@lvt.com>
2022-08-24 23:24:01 -0600

    Stub out messages further

---
+++ lib/warehouse_component/messages/commands/ship_product.rb
@@ -0,0 +1,11 @@
+module WarehouseComponent
+  module Messages
+    module Commands
+      class ShipProduct
+        include Messaging::Message
+
+      end
+    end
+  end
+end
+  
---
+++ lib/warehouse_component/messages/events/product_shipped.rb
@@ -0,0 +1,11 @@
+module WarehouseComponent
+  module Messages
+    module Events
+      class ProductShipped
+        include Messaging::Message
+
+      end
+    end
+  end
+end
+  
